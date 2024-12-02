f4445ba136e63c5ba18b67c020f5b69629ec9035
KennethM19 <ken.jav550@gmail.com>
2024-12-02 10:35:19 -0500

    validaciones del login

--- src/app/gourmet/auth/login/login.component.ts
+++ src/app/gourmet/auth/login/login.component.ts
@@ -1,29 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export default class LoginComponent {
-  username: string = '';
-  password: string = '';
+export default class LoginComponent implements OnInit{
+  formLogin: FormGroup = new FormGroup({});
+  loginError: boolean = false;
 
   constructor(private authService: AuthService, private router: Router){}
+  ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      username: new FormControl('',[
+        Validators.required
+      ]),
+      password: new FormControl('',[
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20)
+      ])
+    })
+  }
 
   login():void {
-    this.authService.login(this.username, this.password).subscribe({
+    const { username, password } = this.formLogin.value;
+    this.authService.login(username, password).subscribe({
       next: () => {
         this.router.navigate(['/dashboard']);
       },
       error: (error) => {
-        console.error(error);
+        this.loginError = true;
+        console.error('Error en las credenciales');
       }
     })
   }
