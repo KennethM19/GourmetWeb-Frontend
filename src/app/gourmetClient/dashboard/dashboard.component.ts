99929422e68c7b726d28fd61543b1cd66b2e202b
brayandand99 <98857234+brayandand99@users.noreply.github.com>
2024-12-03 11:44:14 -0500

    agregado interfaz al dashboard y footer (#14)
    Se agrego cambios el html, css y ts de ambos componentes

--- src/app/gourmetClient/dashboard/dashboard.component.ts
+++ src/app/gourmetClient/dashboard/dashboard.component.ts
@@ -1,12 +1,26 @@
 import {Component} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export default class DashboardComponent {
-
+  platillos = [
+    {
+      imagen: 'https://www.emprenderconactitud.com/img/POC%20WCS%20(1).png',
+      descripcion: 'Delicioso plato gourmet con una presentación artística que resalta los colores vibrantes de los ingredientes.'
+    },
+    {
+      imagen: 'https://www.emprenderconactitud.com/img/POC%20WCS%20(1).png',
+      descripcion: 'Jugoso filete acompañado de una guarnición de verduras asadas, resaltando la frescura y calidad de los ingredientes.'
+    },
+    {
+      imagen: 'https://www.emprenderconactitud.com/img/POC%20WCS%20(1).png',
+      descripcion: 'Ensalada vibrante con verduras frescas y coloridas, perfecta para una experiencia culinaria saludable y deliciosa.'
+    }
+  ];
 }
