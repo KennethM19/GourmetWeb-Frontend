b23aa067dab7abd854cbed3a8c49c25d65006b27
KennethM19 <ken.jav550@gmail.com>
2024-12-02 20:39:24 -0500

    validar el registro

--- src/app/gourmet/auth/login/login.component.ts
+++ src/app/gourmet/auth/login/login.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,27 +15,25 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export default class LoginComponent implements OnInit{
+export default class LoginComponent implements OnInit {
   formLogin: FormGroup = new FormGroup({});
   loginError: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router){}
+  constructor(private authService: AuthService, private router: Router) {}
   ngOnInit(): void {
     this.formLogin = new FormGroup({
-      username: new FormControl('',[
-        Validators.required
-      ]),
-      password: new FormControl('',[
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [
         Validators.required,
         Validators.minLength(8),
-        Validators.maxLength(20)
-      ])
-    })
+        Validators.maxLength(20),
+      ]),
+    });
   }
 
-  login():void {
+  login(): void {
     const { username, password } = this.formLogin.value;
     this.authService.login(username, password).subscribe({
       next: () => {
@@ -38,8 +42,7 @@ export default class LoginComponent implements OnInit{
       error: (error) => {
         this.loginError = true;
         console.error('Error en las credenciales');
-      }
-    })
+      },
+    });
   }
-
 }
--- src/app/gourmet/auth/register/register.component.ts
+++ src/app/gourmet/auth/register/register.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
@@ -7,11 +13,13 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export default class RegisterComponent {
+export default class RegisterComponent implements OnInit {
+  formRegister: FormGroup = new FormGroup({});
+  registerError: boolean = false;
   user = {
     nombres: '',
     apellidos: '',
@@ -19,14 +27,43 @@ export default class RegisterComponent {
     telefono: '',
     username: '',
     password: '',
-    dni: ''
+    dni: '',
   };
 
   constructor(private http: HttpClient, private router: Router) {}
 
+  ngOnInit(): void {
+    this.formRegister = new FormGroup({
+      dni: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^\d{8}$/),
+      ]),
+      nombres: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z]+$/),
+      ]),
+      apellidos: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z]+$/),
+      ]),
+      telefono: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^9\d{8}$/),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(20),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required
+      ])
+    });
+  }
   register(): void {
     const apiUrl = 'https://server.rest.devmb.top/admin-res/api/v1/usuarios/';
-    
+
     this.http.post(apiUrl, this.user).subscribe({
       next: () => {
         console.log('Registro exitoso');
@@ -34,7 +71,7 @@ export default class RegisterComponent {
       },
       error: (error) => {
         console.error('Error durante el registro', error);
-      }
+      },
     });
   }
 }
