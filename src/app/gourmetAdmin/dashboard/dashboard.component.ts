805b4d914a8012748229bb4b87060a4509c6a241
KennethM19 <ken.jav550@gmail.com>
2024-12-03 02:15:25 -0500

    agregar fontawesome

--- src/app/gourmetAdmin/dashboard/dashboard.component.ts
+++ src/app/gourmetAdmin/dashboard/dashboard.component.ts
@@ -1,14 +1,18 @@
 import {Component} from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faBell, faUser} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export default class DashboardComponent {
   menuHidden: boolean = true;
+  faUser = faUser;
+  fabell = faBell
 
   toggleMenu(): void {
     this.menuHidden = !this.menuHidden;
