1e808a8e50e5fa1af2c69ebbf2bc05dff5917cbf
kenneth <126686075+KennethM19@users.noreply.github.com>
2025-05-16 02:45:04 -0500

    Fix/may 16 (#41)
    * fix: upgadre css limit
    
    * fix: add products to the order

--- src/app/gourmetClient/dashboard/dashboard.component.ts
+++ src/app/gourmetClient/dashboard/dashboard.component.ts
@@ -21,15 +21,15 @@ export default class DashboardComponent {
 
   platillos = [
     {
-      imagen: 'https://i.ibb.co/LQSBMKs/comida-1.jpg',
+      imagen: 'https://storage.googleapis.com/www-factornueve-com/2020/02/9eab5278-bigstock-pollock-fish-fillet-with-veget-338718964.jpg',
       descripcion: 'Delicioso plato gourmet con una presentación artística que resalta los colores vibrantes de los ingredientes.'
     },
     {
-      imagen: 'https://i.ibb.co/X8pDnwt/filete.jpg',
+      imagen: 'https://png.pngtree.com/thumb_back/fw800/background/20231107/pngtree-deliciously-grilled-steak-with-exquisite-presentation-on-a-textured-black-background-image_13805507.png',
       descripcion: 'Jugoso filete acompañado de una guarnición de verduras asadas, resaltando la frescura y calidad de los ingredientes.'
     },
     {
-      imagen: 'https://i.ibb.co/DVsQLZG/ensalada.jpg',
+      imagen: 'https://th.bing.com/th/id/OIP.SBqHh-QSguZI0cFtgYHUJgHaE7?cb=iwc2&rs=1&pid=ImgDetMain',
       descripcion: 'Ensalada vibrante con verduras frescas y coloridas, perfecta para una experiencia culinaria saludable y deliciosa.'
     }
   ];
